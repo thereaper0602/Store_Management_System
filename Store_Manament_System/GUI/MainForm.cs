4a23b5f640359665a0ca7a2b92cf1284c7421d56
thereaper0602 <2251012121quang@ou.edu.vn>
2025-03-24 21:30:52 +0700

    create mainform

--- Store_Manament_System/GUI/MainForm.cs
+++ Store_Manament_System/GUI/MainForm.cs
@@ -7,11 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 namespace GUI
 {
-    public partial class Form1: Form
+    public partial class MainForm: Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
